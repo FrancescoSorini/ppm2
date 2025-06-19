37699e542f0c9cdeae6c38f57da2609bbabe11d4
Francesco Sorini <sorini.francesco@gmail.com>
2025-06-19 18:40:24 +0200

    Creati i modelli per post e commenti

--- social_media/posts/models.py
+++ social_media/posts/models.py
@@ -1,2 +1,34 @@
 from django.db import models
+from users.models import CustomUser
 
+
+class Post(models.Model):
+    author = models.ForeignKey(
+        CustomUser,
+        on_delete=models.CASCADE,
+        related_name='posts'
+    )
+    content = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    likes = models.ManyToManyField(CustomUser, related_name='liked_posts', blank=True)
+
+    def __str__(self):
+        return f"Post di {self.author.username} - {self.created_at.strftime('%d-%m-%Y %H:%M:%S')}"
+
+
+class Comment(models.Model):
+    post = models.ForeignKey(
+        Post,
+        on_delete=models.CASCADE,
+        related_name='comments'
+    )
+    author = models.ForeignKey(
+        CustomUser,
+        on_delete=models.CASCADE,
+        related_name='comments'
+    )
+    content = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"Commento di {self.author.username} su Post {self.post.id} - {self.created_at.strftime('%d-%m-%Y %H:%M:%S')}"
