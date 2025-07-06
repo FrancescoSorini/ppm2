c7b7582bbf0b355fab2140d2a75a410c40acc9d0
Francesco Sorini <sorini.francesco@gmail.com>
2025-07-07 01:11:48 +0200

    prova debug

--- social_media/social_media/settings.py
+++ social_media/social_media/settings.py
@@ -24,8 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.getenv("DJANGO_SECRET_KEY", "unsafe-default-secret")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.getenv("DJANGO_DEBUG", "False") == "True"
 
+#DEBUG = os.getenv("DJANGO_DEBUG", "False") == "True"
+DEBUG = True
 ALLOWED_HOSTS = ['*']
 
 # Application definition
